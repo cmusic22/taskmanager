08c93be985201d6ba2eabbd534818e2f39456157
Cmusic22 <ma4123ac@go.minneapolis.edu>
2019-05-14 19:57:10 -0500

    added User object class

---
+++ src/main/java/User.java
@@ -0,0 +1,34 @@
+public class User {
+
+    private int id;
+    private String name;
+
+    public User (int userID, String userName){
+
+        id = userID;
+        name = userName;
+    }
+
+    public int getId{
+        return id;
+    }
+
+    public void setId (int id) {
+        this.id = id;
+    }
+
+    public String getName{
+        return name;
+    }
+
+    public void  setName (String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Project" +
+                "id=" + id +
+                ", name='" + name +"'";
+    }
+}
